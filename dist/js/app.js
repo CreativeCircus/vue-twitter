9235401f7c9804d2ed828b283b88c81b3e1e81c9
Chris Silich <chris.silich@gmail.com>
2020-08-05 15:23:28 -0400

    Initial commit

---
+++ dist/js/app.js
@@ -0,0 +1,41 @@
+"use strict";
+
+var TWITTER_API_BASE = "http://circuslabs.net/~chris.silich/twitter-proxy/twitter-proxy.php";
+
+var router = new VueRouter({
+	routes: [{ path: '', component: HomeComponent }]
+});
+
+var app = new Vue({
+	el: "#app",
+	router: router,
+	data: {},
+	created: function created() {
+		// use created to do initial AJAX lookups
+		this.getTweets();
+	},
+	methods: {
+		getTweets: function getTweets() {
+			var _this = this;
+
+			if (!this.hashtag) {
+				this.tweets = [];
+				return;
+			}
+			var uri = TWITTER_API_BASE + '?op=search_tweets&q=' + encodeURIComponent(this.hashtag);
+			console.log('getTweets', uri);
+			axios.get(uri).then(function (response) {
+				console.log(response);
+				if (response && response.data && response.data.statuses) {
+					_this.tweets = response.data.statuses;
+				} else {
+					console.warn("Valid response from twitter api/proxy, but bad data");
+					_this.tweets = [];
+				}
+			}).catch(function (error) {
+				console.warn(error);
+			});
+		}
+	}
+});
+//# sourceMappingURL=app.js.map
---
+++ src/js/app.js
@@ -0,0 +1,28 @@
+const TWITTER_API_BASE = "http://circuslabs.net/proxies/twitter/";
+
+
+var app = new Vue({
+	el: "#app",
+	data: {
+		tweets: []
+	},
+	methods: {
+		getTweets: function () {
+			var uri = TWITTER_API_BASE + '?op=search_tweets&q=' + encodeURIComponent(this.hashtag);
+			console.log('getTweets', uri);
+			axios.get(uri)
+				.then((response) => {
+					console.log(response);
+					if (response && response.data && response.data.statuses) {
+						this.tweets = response.data.statuses;
+					} else {
+						console.warn("Valid response from twitter api/proxy, but bad data");
+						this.tweets = [];
+					}
+				})
+				.catch((error) => {
+					console.warn(error);
+				});
+		}
+	}
+})
